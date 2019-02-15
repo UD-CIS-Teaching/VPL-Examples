4080021a32ea2086112859c207f07e5c59dc5bdc
Austin Cory Bart <acbart@vt.edu>
2019-02-15 12:25:11 -0500

    Update and rename eval-with-username.sh to eval-with-username/vpl_evaluate.sh

--- eval-with-username/vpl_evaluate.sh
+++ eval-with-username/vpl_evaluate.sh
@@ -6,6 +6,9 @@ echo "#!/bin/bash" >> vpl_execution
 # This line does the magic - note the $MOODLE_USER_NAME variable
 #   also note that I wrapped it in quotes. I had a student with
 #   apostrophes last semester!
+# The fact that I'm passing into a python script I wrote (grader.py) is irrelevant;
+#   the important part is that you have access to this magic variable.
+# The $VPL_VARIATION value is also available if you set up Variations (which are cumbersome)
 echo "python3 grader.py" $VPL_SUBFILE0 $VPL_VARIATION '"'$MOODLE_USER_NAME'"' >> vpl_execution
 
 chmod +x vpl_execution
